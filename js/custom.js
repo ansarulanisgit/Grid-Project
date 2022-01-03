26a18dcd7e3f82dacbef51d62605a8f146b4b2ea
Ansarul Anis <ansarul.contact@gmail.comm>
2022-01-03 10:33:36 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+	new WOW().init();
+	jQuery(".info a").prettyPhoto({
+		animation_speed: 'fast',
+		slideshow: 5000,
+		allow_resize: true,
+		autoplay: true,
+	});
+
+
+	
+});
